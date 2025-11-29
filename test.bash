27d03ca9c753a11d6e780d0ce9ea87af4e071d2a
Daiki Okamoto <s24c1032kz@s.chibakoudai.jp>
2025-11-29 15:48:14 +0900

    updata ver2

--- test.bash
+++ test.bash
@@ -1,23 +1,21 @@
 #!/bin/bash 
-# SPDX-FileCopyrightText: 2025 Daiki Okamoto
-# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-FileCopyrightText: 2025 Daiki Okamoto 
+# SPDX-License-Identifier: BSD-3-Clause 
 
 ng () {
-	echo ${1}行目が違うよ
-	res=1
+    echo "$1行目が違うよ"
+    res=1
 }
-
 res=0
 
-out=$(./kadai - 30 15)
-[ "${out}" = "15" ] || ng "$LINENO"
+out=$(./kadai 2 4)
+[ "${out}" = 8.0 ] || ng "$LINENO"
 
-out=$(echo a | ./kadai)
-[ "$?" = 1 ]
-[ "${out}" = "" ] || ng "$LINENO"
+out=$(./kadai -2 3)
+[ "${out}" = -6.0 ] || ng "$LINENO"
 
-out=$(echo | ./kadai)
-[ "$?" = 1 ]
+out=$(./kadai 2 a 2>/dev/null)
+[ "$?" = 1 ] || ng "$LINENO" 
 [ "${out}" = "" ] || ng "$LINENO"
 
 [ "$res" = 0 ] && echo OK
